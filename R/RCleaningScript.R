58803945c35c05c38ff5ab80e9861f6a0612aca4
newns92 <s.newns92@gmail.com>
2017-01-12 14:46:39 -0500

    Fix switched-name dataset and CSV files, fix typos in run name, create RUN TYPE variable

--- R/RCleaningScript.R
+++ R/RCleaningScript.R
@@ -1,60 +1,66 @@
-#setwd("C:/Users/snewns/Dropbox/RunningAnalysis/Data")
-setwd("C:/Users/Nimz/Dropbox/RunningAnalysis/Data")
+setwd("C:/Users/snewns/Dropbox/RunningAnalysis/Data")
+#setwd("C:/Users/Nimz/Dropbox/RunningAnalysis/Data")
 
-garmin <- read.csv("garmin.csv")
-str(garmin)
-head(garmin)
+strava <- read.csv("strava.csv")
+str(strava)
+head(strava)
 
 #remove unneccessary cols
 keepCols <- NA
 keepCols <- c("Activity.Id", "When", "Type", "Gear", "Name", "Dist.mi", "Elv.ft", "Elapsed.Time", "Moving.Time",
               "Speed.mph", "Pace..mi", "Max.Pace..mi", "Cad", "Heart", "Max.Heart", "Elev.Dist.ft.mi",
               "Elev.Time.ft.h", "Cal", "Segs", "PRs", "Kudos")
-newGarmin <- garmin[keepCols]
-str(newGarmin)
-head(newGarmin)
+newStrava <- strava[keepCols]
+str(newStrava)
+head(newStrava)
+tail(newStrava)
 
-'##PRIOR DATA CLEANING --> REMOVE 2 ROWS AT TOP OF STRAVA FILES
+##PRIOR DATA CLEANING --> REMOVE 2 ROWS AT TOP OF GARMIN FILES
 
-strava1 <- read.csv("strava1.csv")
+'garmin1 <- read.csv("garmin1.csv")
 
-#check for interesting cols from Strava to keep
-str(strava1)
-names(strava1)
+#check for interesting cols from garmin to keep
+str(garmin1)
+names(garmin1)
 #Look at them more closely
-head(strava1)
+head(garmin1)
+tail(garmin1)
 
 #get most recent data
-strava6 <- read.csv("strava6.csv")
-head(strava6)
+garmin6 <- read.csv("garmin6.csv")
+head(garmin6)
+tail(garmin6)
 
-#load in rest of strava files and combine into 1 DF
-strava2 <- read.csv("strava2.csv")
-strava3 <- read.csv("strava3.csv")
-strava4 <- read.csv("strava4.csv")
-strava5 <- read.csv("strava5.csv")
+#load in rest of garmin files and combine into 1 DF
+garmin2 <- read.csv("garmin2.csv")
+garmin3 <- read.csv("garmin3.csv")
+garmin4 <- read.csv("garmin4.csv")
+garmin5 <- read.csv("garmin5.csv")
 
-stravaFull <- Reduce(function(...) merge(..., all=TRUE), list(strava1, strava2, strava3, strava4, 
-                                                              strava5, strava6))
+head(garmin5)
+tail(garmin4)
+
+garminFull <- Reduce(function(...) merge(..., all=TRUE), list(garmin1, garmin2, garmin3, garmin4, 
+                                                              garmin5, garmin6))
 
 #2 random records throwing things off, delete in Excel and reload 
-write.csv(stravaFull, file = "fullStrava.csv", row.names = FALSE)'
-stravaFull <- read.csv("fullStrava.csv")
+#write.csv(garminFull, file = "fullGarmin.csv", row.names = FALSE)'
+garminFull <- read.csv("fullGarmin.csv")
 
-str(stravaFull)
-summary(stravaFull)
+str(garminFull)
+summary(garminFull)
 #multiple runs on August 12? Maybe a bike ride?
-#check type in newGarmin
+#check type in Strava
 
 #install.packages("stringr")
 library(stringr)
-newGarmin$Date <- str_split_fixed(newGarmin$When, " ", 2)[,1]
-newGarmin$StartTime <- str_split_fixed(newGarmin$When, " ", 2)[,2]
+newStrava$Date <- str_split_fixed(newStrava$When, " ", 2)[,1]
+newStrava$StartTime <- str_split_fixed(newStrava$When, " ", 2)[,2]
 
-which(newGarmin$Date == '8/12/2016')
-newGarmin[95:96,]
-#looks like I just ran twice that day in Sea Isle. Back to the Strava data cleaning
-head(stravaFull)
+which(newStrava$Date == '8/12/2016')
+newStrava[95:96,]
+#looks like I just ran twice that day in Sea Isle. Back to the Garmin data cleaning
+head(garminFull)
 
 #install.packages("tidyr")
 library(tidyr)
@@ -62,83 +68,77 @@ library(tidyr)
 vars <- c("Date", "StartTime")
 vars2 <- c("DOW", "Date")
 # then separate the "Details" column according to regex and drop extra columns:
-testStravaFull <- separate(stravaFull, Start, into = vars, sep = "(?<=6 )", extra = "merge", remove = TRUE)
-testStravaFull <- separate(testStravaFull, Date, into = vars2, sep = ", ", extra = "merge", remove = TRUE)
+garminFull <- separate(garminFull, Start, into = vars, sep = "(?<=6 )", extra = "merge", remove = TRUE)
+garminFull <- separate(garminFull, Date, into = vars2, sep = ", ", extra = "merge", remove = TRUE)
 
-testStravaFull[order(testStravaFull$Date),]
+garminFull[order(garminFull$Date),]
 
-testStravaFull$monthNum <- ifelse(grepl("Jan",testStravaFull$Date),1,
-                            ifelse(grepl("Feb",testStravaFull$Date),2,
-                            ifelse(grepl("Mar",testStravaFull$Date),3,
-                            ifelse(grepl("Apr",testStravaFull$Date),4,
-                            ifelse(grepl("May",testStravaFull$Date),5,
-                            ifelse(grepl("Jun",testStravaFull$Date),6,
-                            ifelse(grepl("Jul",testStravaFull$Date),7,
-                            ifelse(grepl("Aug",testStravaFull$Date),8,
-                            ifelse(grepl("Sep",testStravaFull$Date),9,
-                            ifelse(grepl("Oct",testStravaFull$Date),10,
-                            ifelse(grepl("Nov",testStravaFull$Date),11,
-                            ifelse(grepl("Dec",testStravaFull$Date),12,NA))))))))))))
-head(testStravaFull)
+garminFull$monthNum <- ifelse(grepl("Jan",garminFull$Date),1,
+                            ifelse(grepl("Feb",garminFull$Date),2,
+                            ifelse(grepl("Mar",garminFull$Date),3,
+                            ifelse(grepl("Apr",garminFull$Date),4,
+                            ifelse(grepl("May",garminFull$Date),5,
+                            ifelse(grepl("Jun",garminFull$Date),6,
+                            ifelse(grepl("Jul",garminFull$Date),7,
+                            ifelse(grepl("Aug",garminFull$Date),8,
+                            ifelse(grepl("Sep",garminFull$Date),9,
+                            ifelse(grepl("Oct",garminFull$Date),10,
+                            ifelse(grepl("Nov",garminFull$Date),11,
+                            ifelse(grepl("Dec",garminFull$Date),12,NA))))))))))))
+head(garminFull)
 
 vars3 <- c("Month", "Date")
-testStravaFull <- separate(testStravaFull, Date, into = vars3, sep = " ", extra = "merge", remove = TRUE)
-head(testStravaFull)
+garminFull <- separate(garminFull, Date, into = vars3, sep = " ", extra = "merge", remove = TRUE)
+head(garminFull)
 vars4 <- c("Day", "Year")
-testStravaFull <- separate(testStravaFull, Date, into = vars4, sep = ", ", extra = "merge", remove = TRUE)
-head(testStravaFull)
+garminFull <- separate(garminFull, Date, into = vars4, sep = ", ", extra = "merge", remove = TRUE)
+head(garminFull)
 
-#testDates <- as.Date(with(testStravaFull, paste(Year, monthNum, Day,sep="-")), "%Y-%m-%d")
-#as.Date(with(testStravaFull, paste(Year, monthNum, Day,sep="/")), "%m/%d/%Y")
-#testStravaFull$Day <- as.numeric(testStravaFull$Day)
-#testStravaFull$Year <- as.numeric(testStravaFull$Day)
+garminFull$Year <- trimws(garminFull$Year)
+garminFull$Date <- format(as.Date(with(garminFull, paste(Year, monthNum, Day,sep="-")), "%Y-%m-%d"), "%m/%d/%Y")
+str(garminFull$Date)
 
-testStravaFull$Year <- trimws(testStravaFull$Year)
-testStravaFull$Date <- format(as.Date(with(testStravaFull, paste(Year, monthNum, Day,sep="-")), "%Y-%m-%d"), "%m/%d/%Y")
-str(testStravaFull$Date)
-
-keepColsStrava  <- NA
-keepColsStrava <- c("DOW", "Month", "StartTime", "Time", "Distance", "Elevation.Gain", "Avg.Speed.Avg.Pace.", "Avg.HR", 
+keepColsGarmin  <- NA
+keepColsGarmin <- c("DOW", "Month", "StartTime", "Time", "Distance", "Elevation.Gain", "Avg.Speed.Avg.Pace.", "Avg.HR", 
                     "Max.HR", "Calories", "Date", "monthNum")
-newStrava <- testStravaFull[keepColsStrava]
-head(newStrava)
+newGarmin <- garminFull[keepColsGarmin]
+head(newGarmin)
 
 newGarmin$Date <- as.Date(newGarmin$Date,"%m/%d/%Y")
 newStrava$Date <- as.Date(newStrava$Date,"%m/%d/%Y")
-str(newStrava$Date)
 str(newGarmin$Date)
+str(newStrava$Date)
 
+newGarmin <- newGarmin[order(newGarmin$Date, decreasing = FALSE),] 
 newStrava <- newStrava[order(newStrava$Date, decreasing = FALSE),]
-newGarmin <- newGarmin[order(newGarmin$Date, decreasing = FALSE),] #3 extra runs in Jan 2016, remove
-head(newGarmin)#$Gear)
+head(newGarmin)
 head(newStrava)
 
+#3 extra Garmin runs in Jan 2016, remove
 summary(newGarmin$Date)
-
-#newGarmin$Date <- format(newGarmin$Date, "%m/%d/%Y")
-
 
 newGarmin <- newGarmin[!newGarmin$Date == c("2016-01-20","2016-01-30","2016-01-31")]
 #newGarmin <- newGarmin[!newGarmin$Date == "2016-01-30",]
 #newGarmin <- newGarmin[!newGarmin$Date == "2016-01-31",]
 newGarmin <- newGarmin[!newGarmin$Date < "2016-07-18",]
+newStrava <- newStrava[!newStrava$Date < "2016-07-18",]
 
-nrow(newGarmin)
-nrow(newStrava) #3 extra
+nrow(newGarmin) #3 extra
+nrow(newStrava)
 
 tail(newGarmin)
 tail(newStrava)
 
+##check in excel (how in R?)
 #write.csv(newStrava$Date, file = "fullStravaDates.csv", row.names = FALSE)
 #write.csv(newGarmin$Date, file = "fullGarminDates.csv", row.names = FALSE)
 
-#check in excel (how in R?)
 #remove false runs
-newStrava <- newStrava[!newStrava$Date == "2016-08-27",]
-newStrava <- newStrava[!newStrava$Date == "2016-09-01",]
-newStrava[newStrava$Date == "2016-10-23",]
+newGarmin <- newGarmin[!newGarmin$Date == "2016-08-27",]
+newGarmin <- newGarmin[!newGarmin$Date == "2016-09-01",]
+newGarmin[newGarmin$Date == "2016-10-23",]
 #remove bike ride
-newStrava <- newStrava[!(newStrava$Date == "2016-10-23" & newStrava$Elevation.Gain == 657),]
+newGarmin <- newGarmin[!(newGarmin$Date == "2016-10-23" & newGarmin$Elevation.Gain == 657),]
 
 nrow(newGarmin)
 nrow(newStrava) #match now
@@ -147,13 +147,13 @@ head(newGarmin)
 head(newStrava)
 #install.packages("plyr") #for rename cols
 library(plyr)
-newStrava <- rename(newStrava, c("Date"="date_strava"))
-newStrava <- rename(newStrava, c("StartTime"="StartTime_AM_PM"))
-newGarmin$When <- NULL
+newGarmin <- rename(newGarmin, c("Date"="date_garmin"))
+newGarmin <- rename(newGarmin, c("StartTime"="StartTime_AM_PM"))
+newStrava$When <- NULL
 
 newFullData <- cbind(newGarmin,newStrava)
 head(newFullData)
-newFullData[,c("Date","date_strava")]
+newFullData[,c("Date","date_garmin")]
 #dates match
 
 newFullData <- rename(newFullData, c("Activity.Id"="ID"))
@@ -166,23 +166,23 @@ newFullData <- newFullData[keepColsFull]
 newFullData <- newFullData[, c("ID", "Name", "Gear", "Date", "Month", "monthNum", "DOW", "StartTime",  "Distance", 
                                "Time", "Avg.Speed.Avg.Pace.", "Speed.mph", "Cad",  "Elevation.Gain", "Avg.HR", "Max.HR", 
                                "Calories")]
-newFullData$Type <- ?ifelse(?is.element('Middle Long' %in% newFullData$Name),"yes","no")
-table(newFullData$Name)
+#newFullData$Type <- ?ifelse(?is.element('Middle Long' %in% newFullData$Name),"yes","no")
+table(newFullData$Name) 
 
-newFullData$Name <- if (newFullData$Name == "MIddle Long Run") {
-                              newFullData$Name <- "Middle Long Run"
-                      }
-newFullData$Name <- ifelse(grepl("ML Run",newFullData$Name),"Middle Long Run",newFullData$Name)
+#write.csv(newFullData, file = "newFullData.csv", row.names = FALSE)
+str(newFullData$ID)
+#Fix typos in names
+newFullData$Name[newFullData$Name == 'MIddle Long Run'] <- 'ML Run'
+newFullData$Name[newFullData$Name == 'Middle Long Run'] <- 'ML Run'
+newFullData[newFullData$Name == 'Morning Run',] #One recovery run and one ML run based off of distance and pace
+newFullData$Name[newFullData$ID == 650512799] <- 'Recovery Run'
+newFullData$Name[newFullData$ID == 655096239] <- 'ML Run'
 
-newFullData$Type <- ifelse(grepl(c("V02","Tempo","Tune Up","LT","Tempo"),testStravaFull$Date),"Workout",
-                                  ifelse(grepl("Long",testStravaFull$Date),"Long",
-                                         ifelse(grepl("Middle","MIddle","ML")testStravaFull$Date),"Run",
-                                                ifelse(grepl("Recovery",testStravaFull$Date),"Recovery",
-                                                       ifelse(grepl("May",testStravaFull$Date),5,
-                                                              ifelse(grepl("Jun",testStravaFull$Date),6,
-                                                                     ifelse(grepl("Jul",testStravaFull$Date),7,
-                                                                            ifelse(grepl("Aug",testStravaFull$Date),8,
-                                                                                   ifelse(grepl("Sep",testStravaFull$Date),9,
-                                                                                          ifelse(grepl("Oct",testStravaFull$Date),10,
-                                                                                                 ifelse(grepl("Nov",testStravaFull$Date),11,
-                                                                                                        ifelse(grepl("Dec",testStravaFull$Date),12,NA))))))))))))
+#create new "run type" for run, recovery, long, workout etc.
+newFullData$RunType <- ifelse(grepl('LT',newFullData$Name),'Workout',
+                        ifelse(grepl('Tempo',newFullData$Name),'Workout',
+                          ifelse(grepl('Tune',newFullData$Name),'Workout',
+                            ifelse(grepl('V02',newFullData$Name),'Workout',
+                              ifelse(grepl('Long',newFullData$Name),'Long Run',
+                                ifelse(grepl('Recovery',newFullData$Name),'Recovery Run','Run'))))))
+table(newFullData$RunType)
