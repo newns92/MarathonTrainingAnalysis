c632c213c72e2cf7d2d9cd9fba00a74034791ee7
Nimz <s.newns92@gmail.com>
2017-01-23 17:45:43 -0500

    Create 2 ggplot plots of distance histrogram and miles per month

--- R/R_Analysis.R
+++ R/R_Analysis.R
@@ -15,8 +15,16 @@ str(runs)
 
 library(ggplot2)
 
-?barplot(sum(runs$Distance),runs$monthNum)
+#?barplot(sum(runs$Distance),runs$monthNum)
 
-ggplot(data = runs, aes(x = Distance)) + geom_histogram(binwidth = 2)
+ggplot(data = runs, aes(x = Distance)) + geom_histogram(binwidth = 2,aes(fill = ..count..))
+#majority of miles between 9-11 miles
+
+ggplot(data = runs, aes(x = monthNum, y = Distance)) + geom_bar(stat="identity")
+#most miles in August, not OCtober, still more in November than in June
+
+
+as.date(runs$Avg.Speed.Avg.Pace.,'m:s')
+as.Date.(runs$Avg.Speed.Avg.Pace.,'m:s')
 
 sum(runs$Distance)
